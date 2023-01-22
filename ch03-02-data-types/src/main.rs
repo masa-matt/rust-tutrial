8abff84fcacec87962800f02cc9b3b56744359b4
masa-matt <masatoshi.ito@hashport.io>
2023-01-22 20:45:21 +0900

    ch03 02

--- ch03-02-data-types/src/main.rs
+++ ch03-02-data-types/src/main.rs
@@ -1,3 +1,127 @@
+use std::io;
+
 fn main() {
+    println!("* Data Types *");
+    // error[E0282]: type annotations needed
+    // let guess = "42".parse().expect("Not a number!");
     let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of guess is: {guess}");
+
+    println!();
+    println!("* Floating-Point Types *");
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+
+    println!();
+    println!("* Numeric Operations *");
+    // addition
+    let sum = 5 + 10;
+    println!("The value of sum is: {sum}");
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("The value of difference is: {difference}");
+
+    // multiplication
+    let product = 4 * 30;
+    println!("The value of product is: {product}");
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+    println!("The value of quotien is: {quotient}");
+    println!("The value of truncated is: {truncated}");
+
+    // remainder
+    let remainder = 43 % 5;
+    println!("The value of remainder is: {remainder}");
+
+    println!();
+    println!("* The Boolean Type *");
+    let t = true;
+    let f: bool = false; // with explicit type annotation
+    println!("The value of t is: {t}");
+    println!("The value of f is: {f}");
+
+    println!();
+    println!("* The Character Type *");
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+    println!("The value of c is: {c}");
+    println!("The value of z is: {z}");
+    println!("The value of heart_eyed_cat is: {heart_eyed_cat}");
+
+    println!();
+    println!("* The Tuple Type *");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x is: {x}");
+    println!("The value of y is: {y}");
+    println!("The value of z is: {z}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+    println!("The value of five_hundred is: {five_hundred}");
+    println!("The value of six_point_four is: {six_point_four}");
+    println!("The value of one is: {one}");
+
+    println!();
+    println!("* The Array Type *");
+    let a = [1, 2, 3, 4, 5];
+    println!("The value of a is: {:?}", a);
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    println!("The value of months is: {:?}", months);
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("The value of a is: {:?}", a);
+    let a = [3; 5];
+    println!("The value of a is: {:?}", a);
+
+    println!();
+    println!("* Accessing Array Elements *");
+    let a = [1, 2, 3, 4, 5];
+    let first = a[0];
+    let second = a[1];
+    println!("The value of first is: {first}");
+    println!("The value of second is: {second}");
+
+    println!();
+    println!("* Invalid Array Element Access *");
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
